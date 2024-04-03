6c682161a31d39ee76b1e18207bf8d2c0a68ab64
Niloo Mont <niloo.mon@gmail.com>
2024-04-03 13:26:00 +0330

    fix: Add exception handling to EventConsumer

--- Twitter.Clone.Trends/EventHandler/EventConsumer.cs
+++ Twitter.Clone.Trends/EventHandler/EventConsumer.cs
@@ -1,20 +1,31 @@
 ﻿namespace Twitter.Clone.Trends.EventHandler;
 
-public class EventConsumer(HashtagsService hashtagsService) : IConsumer<EventData>
+public class EventConsumer(HashtagsService hashtagsService,
+    ILogger<EventConsumer> logger)
+    : IConsumer<EventData>
 {
     private readonly HashtagsService _hashtagsService = hashtagsService;
+    private readonly ILogger<EventConsumer> _logger = logger;
 
     public async Task Consume(ConsumeContext<EventData> context)
     {
-        if (!string.IsNullOrEmpty(context.Message.IPAddress) &&
-            context.Message.Hashtags.Count > 0)
-            foreach (var hashtag in context.Message.Hashtags)
-                await _hashtagsService.CreateAsync(new Hashtag
-                {
-                    IPAddress = context.Message.IPAddress,
-                    Name = hashtag,
-                    DateCreated = DateTime.UtcNow,
-                    IsProcessed = false,
-                });
+        try
+        {
+            if (!string.IsNullOrEmpty(context.Message.IPAddress) &&
+                context.Message.Hashtags.Count > 0)
+                foreach (var hashtag in context.Message.Hashtags)
+                    await _hashtagsService.CreateAsync(new Hashtag
+                    {
+                        IPAddress = context.Message.IPAddress,
+                        Name = hashtag,
+                        DateCreated = DateTime.UtcNow,
+                        IsProcessed = false,
+                    });
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, 
+                $"Error occurred while processing the message with IP: {context.Message.IPAddress}");
+        }
     }
 }
