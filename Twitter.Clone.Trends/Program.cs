fdcdd485becb3838bf5dd7d3f1f74d5c6f0a346d
Niloo Mont <niloo.mon@gmail.com>
2024-04-03 12:24:01 +0330

    fix: Change project to web api

--- Twitter.Clone.Trends/Program.cs
+++ Twitter.Clone.Trends/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+
+app.Run();
