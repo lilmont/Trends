17e325ac9dc3bfb39ff8fe47cda64d0f68e2660e
Morvarid Arab <m.arab@avang.ir>
2024-04-15 00:23:54 +0330

    Add HashtagLogs collection and needed CRUD operations

--- Twitter.Clone.Trends/Services/HashtagRepository.cs
+++ Twitter.Clone.Trends/Services/HashtagRepository.cs
@@ -3,6 +3,8 @@
 public class HashtagRepository
 {
     private readonly IMongoCollection<Hashtag> _hashtagsCollection;
+    private readonly IMongoCollection<HashtagLog> _hashtagLogsCollection;
+
     private readonly static InsertOneOptions _insertOneOptions = new();
 
     public HashtagRepository(
@@ -16,8 +18,24 @@ public class HashtagRepository
 
         _hashtagsCollection = mongoDatabase.GetCollection<Hashtag>(
             Hashtag.CollectionName);
+        _hashtagLogsCollection = mongoDatabase.GetCollection<HashtagLog>(
+            HashtagLog.CollectionName);
     }
 
-    public async Task CreateAsync(Hashtag newHashtag, CancellationToken cancellationToken) =>
+    public async Task CreateHashtagAsync(Hashtag newHashtag, CancellationToken cancellationToken) =>
         await _hashtagsCollection.InsertOneAsync(newHashtag, _insertOneOptions, cancellationToken);
+    public async Task<List<Hashtag>> GetAllHashtagsAsync() =>
+        await _hashtagsCollection.Find(_ => true).ToListAsync();
+    public async Task<Hashtag> GetHashtagByIdAsync(string id) =>
+        await _hashtagsCollection.Find(hashtag  => hashtag.Id == id).FirstOrDefaultAsync();
+    public async Task<Hashtag> GetHashtagByNameAsync(string name) =>
+        await _hashtagsCollection.Find(hashtag => hashtag.Name == name).FirstOrDefaultAsync();
+
+
+    public async Task CreateLogAsync(HashtagLog newLog) =>
+        await _hashtagLogsCollection.InsertOneAsync(newLog);
+    public async Task<List<HashtagLog>> GetAllLogsAsync() =>
+        await _hashtagLogsCollection.Find(_ => true).ToListAsync();
+    public async Task<HashtagLog> GetLogByIdAsync(string id) =>
+        await _hashtagLogsCollection.Find(hashtag => hashtag.Id == id).FirstOrDefaultAsync();
 }
