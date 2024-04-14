82ea0fdba7efc181f86da74d04b353f76ea69d75
Morvarid Arab <m.arab@avang.ir>
2024-04-15 00:21:52 +0330

    Add count property to hashtagLog entity

--- Twitter.Clone.Trends/Models/Entities/Hashtag.cs
+++ Twitter.Clone.Trends/Models/Entities/Hashtag.cs
@@ -7,9 +7,12 @@ public class Hashtag
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = string.Empty;
-    public required string Name { get; set; }
+
+    [BsonRequired]
+    public string Name { get; set; }
 
     public List<HashtagLog>? Log { get; set; }
+
 }
 
 public class HashtagLog
@@ -20,9 +23,17 @@ public class HashtagLog
     [BsonRepresentation(BsonType.ObjectId)]
     public required string Id { get; set; }
 
-    public required string HashtagId { get; set; }
-    public required DateTime DateCreated { get; set; }
-    public required string IPAddress { get; set; }
+    [BsonRequired]
+    public string HashtagId { get; set; }
+
+    [BsonRequired]
+    public DateTime DateCreated { get; set; }
+
+    [BsonRequired]
+    public string IPAddress { get; set; }
+
     public bool IsProcessed { get; set; }
+
+    public int Count { get; set; }
 }
 
