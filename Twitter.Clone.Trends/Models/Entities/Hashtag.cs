09480b619fa555f2749eb2fb0e7d20615ac56d2b
Morvarid Arab <m.arab@avang.ir>
2024-04-14 17:41:53 +0330

    Add HashtagLog entity. Add list of HashtagLogs to Hashtag entity.

--- Twitter.Clone.Trends/Models/Entities/Hashtag.cs
+++ Twitter.Clone.Trends/Models/Entities/Hashtag.cs
@@ -8,6 +8,19 @@ public class Hashtag
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = string.Empty;
     public required string Name { get; set; }
+
+    public List<HashtagLog>? Log { get; set; }
+}
+
+public class HashtagLog
+{
+    public const string CollectionName = "HashtagLogs";
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; set; }
+
+    public required string HashtagId { get; set; }
     public required DateTime DateCreated { get; set; }
     public required string IPAddress { get; set; }
     public bool IsProcessed { get; set; }
